e7e3e7ec7e42189d88a49334709e7e8af110bdbb
Somenath Choudhury <somenathchoudhury35@gmail.com>
2023-04-13 15:30:46 +0530

    commited

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-import pandas as pd 
+import pandas as pd
 import pickle
 
 
@@ -20,7 +20,6 @@ app.add_middleware(
 )
 
 
-
 model = pickle.load(open('predict_student_dropout_academic_success.pkl', 'rb'))
 
 
@@ -33,34 +32,33 @@ class StudentInfoModel(BaseModel):
     educational_special_needs: int
     tution_fees_upto_date: int
     gender: int
-    scholarship_holder: int 
-    age_of_enrollement: int 
+    scholarship_holder: int
+    age_of_enrollement: int
     cirricular_units_first_unit_sem_credited: int
-    cirricular_units_first_unit_sem_enrolled: int 
-    cirricular_units_first_unit_sem_evaluations: int 
+    cirricular_units_first_unit_sem_enrolled: int
+    cirricular_units_first_unit_sem_evaluations: int
     cirricular_units_first_unit_sem_approved: int
-    cirricular_units_first_unit_sem_grade: int 
-    cirricular_units_first_unit_sem_without_evaluations: int 
-    cirricular_units_second_unit_sem_credited: int 
-    cirricular_units_second_unit_sem_enrolled: int 
-    cirricular_units_second_unit_sem_evaluations: int 
-    cirricular_units_second_unit_sem_approved: int 
-    cirricular_units_second_unit_sem_grade: int 
+    cirricular_units_first_unit_sem_grade: int
+    cirricular_units_first_unit_sem_without_evaluations: int
+    cirricular_units_second_unit_sem_credited: int
+    cirricular_units_second_unit_sem_enrolled: int
+    cirricular_units_second_unit_sem_evaluations: int
+    cirricular_units_second_unit_sem_approved: int
+    cirricular_units_second_unit_sem_grade: int
     cirricular_units_second_unit_sem_without_evaluations: int
-
 
 
 @app.get('/')
 async def welcome_msg():
     return {
-        'success' : True,
-        'message' : 'The server of "predict student dropout and academic success" is running successfully'
-    } 
-    
-    
+        'success': True,
+        'message': 'The server of "predict student dropout and academic success" is running successfully'
+    }
+
+
 @app.post('/predict')
 async def predict(studentInfo: StudentInfoModel):
-    course= studentInfo.course_name
+    course = studentInfo.course_name
     daytimeEveningAttendance = studentInfo.daytime_evening_attendance
     previousQualification = studentInfo.prev_qualification
     previousQualificationGrade = studentInfo.prev_qualification_grade
@@ -81,16 +79,25 @@ async def predict(studentInfo: StudentInfoModel):
     cirricularUnitSecondSemEvaluation = studentInfo.cirricular_units_second_unit_sem_evaluations
     cirricularUnitSecondSemApproved = studentInfo.cirricular_units_second_unit_sem_approved
     cirricularUnitSecondSemGrade = studentInfo.cirricular_units_second_unit_sem_grade
-    cirricularUnitSecondSemWithoutEvaluation = studentInfo.cirricular_units_second_unit_sem_without_evaluations 
+    cirricularUnitSecondSemWithoutEvaluation = studentInfo.cirricular_units_second_unit_sem_without_evaluations
 
-    dataFrameOfTheUserInputData = pd.DataFrame([[course, daytimeEveningAttendance, previousQualification, previousQualificationGrade, admissionGrade, educationSpecialNeeds, tutionFeesUptoDate, gender, scholarshipHolder, ageOfEnrollement, cirricularUnitFirstSemCredited, cirricularUnitFirstSemEnrolled, cirricularUnitFirstSemEvaluation, cirricularUnitFirstSemApproved, cirricularUnitFirstSemGrade, cirricularUnitFirstSemWithoutEvalutaion, cirricularUnitSecondSemCredited, cirricularUnitSecondSemEnrolled, cirricularUnitSecondSemEvaluation, cirricularUnitSecondSemApproved, cirricularUnitSecondSemGrade, cirricularUnitSecondSemWithoutEvaluation]], 
+    dataFrameOfTheUserInputData = pd.DataFrame([[course, daytimeEveningAttendance, previousQualification, previousQualificationGrade, admissionGrade, educationSpecialNeeds, tutionFeesUptoDate, gender, scholarshipHolder, ageOfEnrollement, cirricularUnitFirstSemCredited, cirricularUnitFirstSemEnrolled, cirricularUnitFirstSemEvaluation, cirricularUnitFirstSemApproved, cirricularUnitFirstSemGrade, cirricularUnitFirstSemWithoutEvalutaion, cirricularUnitSecondSemCredited, cirricularUnitSecondSemEnrolled, cirricularUnitSecondSemEvaluation, cirricularUnitSecondSemApproved, cirricularUnitSecondSemGrade, cirricularUnitSecondSemWithoutEvaluation]],
                                                columns=['Course', 'Daytime/evening attendance\t', 'Previous qualification', 'Previous qualification (grade)', 'Admission grade', 'Educational special needs', 'Tuition fees up to date', 'Gender', 'Scholarship holder', 'Age at enrollment', 'Curricular units 1st sem (credited)', 'Curricular units 1st sem (enrolled)', 'Curricular units 1st sem (evaluations)', 'Curricular units 1st sem (approved)', 'Curricular units 1st sem (grade)', 'Curricular units 1st sem (without evaluations)', 'Curricular units 2nd sem (credited)', 'Curricular units 2nd sem (enrolled)', 'Curricular units 2nd sem (evaluations)', 'Curricular units 2nd sem (approved)', 'Curricular units 2nd sem (grade)', 'Curricular units 2nd sem (without evaluations)'])
-    
+
     prediction = model.predict(dataFrameOfTheUserInputData)
 
+    resultMsg = ''
+
+    if prediction[0] == 0:
+        resultMsg = 'We are sorry to say but you have a high chance of getting dropout from academics in future.'
+    elif prediction[0] == 1:
+        resultMsg = 'We are glad to say that you have a high chance of success in your academics in future.'
+    elif prediction[0] == 2:
+        resultMsg = 'We are glad to tell you that you will be enrolled in an educational institution in future.'
 
     return {
         'success': True,
         'message': 'Your future academic performance has been predicted successfully',
-        'data': str(prediction[0])
+        'data': str(prediction[0]),
+        'resMessage': resultMsg
     }
